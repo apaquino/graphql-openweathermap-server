1a31262e57c75b84c734480ccfa125c9a47f0635
Armand <apaquino@gmail.com>
2016-01-26 13:10:42 -0800

    destructure new graphql fields

--- components/WeatherListItem.js
+++ components/WeatherListItem.js
@@ -15,15 +15,23 @@ const propTypes = {
 };
 
 const WeatherListItem = ({cityData, deleteCity}) => {
-  const { pressure_data, humidity_data, temp_f_data } = cityData;
+  const {
+    pressure_data,
+    humidity_data,
+    temp_f_data,
+    temp_f_avg,
+    humidity_avg,
+    pressure_avg,
+  } = cityData;
+
   const { lat, lng } = cityData.city.coord;
 
   return (
     <tr>
       <td className="googlemap"><GoogleCityMap lat={lat} lng={lng} /></td>
-      <td><SparkLineChart data={temp_f_data} color="red" avgData={cityData.temp_f_avg}un its="&deg;F" /></td>
-      <td><SparkLineChart data={pressure_data} color="blue" avgData={cityData.pressure_avg}units="hPa"/></td>
-      <td><SparkLineChart data={humidity_data} color="orange" avgData={cityData.humidity_avg} units="%"/></td>
+      <td><SparkLineChart data={temp_f_data} color="red" avgData={temp_f_avg}un its="&deg;F" /></td>
+      <td><SparkLineChart data={pressure_data} color="blue" avgData={pressure_avg}units="hPa"/></td>
+      <td><SparkLineChart data={humidity_data} color="orange" avgData={humidity_avg} units="%"/></td>
       <td><button className="btn btn-danger"
                   onClick={() => deleteCity(cityData.city.id)}
           >
